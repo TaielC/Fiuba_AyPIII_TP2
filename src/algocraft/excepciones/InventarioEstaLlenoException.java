f14b6d520b0a453af655388ff2042ce5906a353b
TaielC <43725160+TaielC@users.noreply.github.com>
2019-06-30 14:53:47 -0300

    Agregadas algunas funciones

--- src/algocraft/excepciones/InventarioEstaLlenoException.java
+++
@@ -1,7 +0,0 @@
-package algocraft.excepciones;
-
-public class InventarioEstaLlenoException extends RuntimeException {
-    public InventarioEstaLlenoException(){
-        super("El inventario Está Lleno");
-    }
-}
---
+++ src/controller/juego/BotonConstruirHerramientaEventHandler.java
@@ -0,0 +1,38 @@
+package controller.juego;
+
+import algocraft.excepciones.InventarioHerramientasEstaLlenoException;
+import algocraft.herramienta.Herramienta;
+import algocraft.herramienta.HerramientaNula;
+import algocraft.juego.Jugador;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import view.PantallaConstructor;
+
+public class BotonConstruirHerramientaEventHandler implements EventHandler<ActionEvent>  {
+
+    private PantallaConstructor pantallaConstructor;
+    private Herramienta herramienta;
+
+    public BotonConstruirHerramientaEventHandler(PantallaConstructor pantallaConstructor, Herramienta herramienta) {
+        this.pantallaConstructor = pantallaConstructor;
+        this.herramienta = herramienta;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if(herramienta instanceof HerramientaNula) return;
+        Jugador jugador = pantallaConstructor.getJuego().getJugador();
+        try{
+            jugador.agregarAInventario(herramienta);
+        } catch (InventarioHerramientasEstaLlenoException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("");
+            alert.setHeaderText("El Inventario de Herramientas está Lleno, no se puede agregar");
+            alert.show();
+            return;
+        }
+        jugador.actualizarInventarioMateriales(pantallaConstructor.getInventarioCopiado().copy());
+        pantallaConstructor.getPantallaJuego().actualizar();
+    }
+}
